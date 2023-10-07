b3abc83593f05dd0a14ddbeca8e8915b261991c7
David Lai <laidav@gmail.com>
2023-10-07 15:23:40 -0400

    Revert "import zonejs"
    This reverts commit cc87c88ea99f3bbe134151a83c75b405cfd3c207.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
-import 'zone.js';
-import 'zone.js/dist/long-stack-trace-zone.js';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch((err) => console.error(err));
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
