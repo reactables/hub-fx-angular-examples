cc87c88ea99f3bbe134151a83c75b405cfd3c207
David Lai <laidav@gmail.com>
2023-10-07 14:51:05 -0400

    import zonejs

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
+import 'zone.js';
+import 'zone.js/dist/long-stack-trace-zone.js';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
