f1b06d2e68f5c8ac07c66384cd86b890d7895ba7
David Lai <laidav@gmail.com>
2023-10-08 10:40:54 -0400

    define actions

--- src/app/event-tickets/event-tickets.component.ts
+++ src/app/event-tickets/event-tickets.component.ts
@@ -1,5 +1,89 @@
 import { Component } from '@angular/core';
-import { Action, Reducer } from '@hub-fx/core';
+import { switchMap, map } from 'rxjs/operators';
+import { Action, Reducer, HubFactory } from '@hub-fx/core';
+import { EventTypes, FetchPricePayload } from '../Models/EventTypes';
+import { TicketService } from '../ticket.service';
+import { Observable } from 'rxjs';
+
+// Actions
+export const SELECT_EVENT = 'SELECT_EVENT';
+export const selectEvent = (event: EventTypes): Action<EventTypes> => ({
+  type: SELECT_EVENT,
+  payload: event,
+});
+
+export const SET_QTY = 'SET_QTY';
+export const setQty = (qty: number): Action<number> => ({
+  type: SET_QTY,
+  payload: qty,
+});
+
+export const FETCH_PRICE_SUCCESS = 'FETCH_PRICE_SUCCESS';
+export const fetchPriceSuccess = (price: number) => ({
+  type: FETCH_PRICE_SUCCESS,
+  payload: price,
+});
+
+export const FETCH_PRICE = 'FETCH_PRICE';
+export const fetchPrice = (
+  payload: FetchPricePayload,
+  getPrice: (payload: FetchPricePayload) => Observable<number>
+): Action<FetchPricePayload> => ({
+  type: FETCH_PRICE,
+  payload,
+  scopedEffects: {
+    effects: [
+      (actions$) =>
+        actions$.pipe(
+          switchMap(({ payload }) => getPrice(payload as FetchPricePayload)),
+          map((price) => fetchPriceSuccess(price))
+        ),
+    ],
+  },
+});
+
+// Reducers
+interface ControlState {
+  selectedEvent: EventTypes | null;
+  qty: number;
+}
+
+const initialControlState: ControlState = {
+  selectedEvent: null,
+  qty: 0,
+};
+
+const controlReducer: Reducer<ControlState> = (
+  state = initialControlState,
+  action
+) => {
+  switch (action?.type) {
+    case SELECT_EVENT:
+      return {
+        ...state,
+        selectedEvent: action.payload as EventTypes,
+      };
+    case SET_QTY:
+      return {
+        ...state,
+        qty: action.payload as number,
+      };
+    default:
+      return state;
+  }
+};
+
+interface PriceState {
+  calculating: boolean;
+  price: number | null;
+}
+
+const initialPriceState = {
+  calculating: false,
+  price: null,
+};
+
+const priceReducer: Reducer<PriceState> = (state = initialPriceState) => {};
 
 @Component({
   selector: 'app-event-tickets',
--- src/app/ticket.service.ts
+++ src/app/ticket.service.ts
@@ -1,9 +1,21 @@
+import { of } from 'rxjs';
+import { delay } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
+import { EventTypes, FetchPricePayload } from './Models/EventTypes';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TicketService {
+  private prices = {
+    [EventTypes.ChiliCookOff]: 20,
+    [EventTypes.ItchyAndScratchyMovie]: 40,
+    [EventTypes.GrammarRodeo]: 50,
+  };
 
-  constructor() { }
+  constructor() {}
+
+  getPrice({ event, qty }: FetchPricePayload) {
+    return of(this.prices[event] * qty).pipe(delay(1000));
+  }
 }
