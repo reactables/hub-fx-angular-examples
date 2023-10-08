3a14b651944299cea23dff33982bf3927346310b
David Lai <laidav@gmail.com>
2023-10-08 09:55:56 -0400

    initiate component and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EventTicketsComponent } from './event-tickets/event-tickets.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, EventTicketsComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/event-tickets/event-tickets.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { Action, Reducer } from '@hub-fx/core';
+
+@Component({
+  selector: 'app-event-tickets',
+  templateUrl: './event-tickets.component.html',
+  styleUrls: ['./event-tickets.component.scss'],
+})
+export class EventTicketsComponent {}
