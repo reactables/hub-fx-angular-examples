b5a1d1007c0e9ff489609523df02348eaae7ec1a
David Lai <laidav@gmail.com>
2023-11-12 10:56:24 -0500

    comment

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { Counter } from '@hub-fx/examples';
 
+// See for implementation
+// https://github.com/hub-fx/hub-fx/tree/main/packages/examples/src/Counter
+
 @Component({
   selector: 'app-counter',
   templateUrl: './counter.component.html',
