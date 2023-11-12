92a090d47a9945f4d3e7badfefd00e9674d6cfca
David Lai <laidav@gmail.com>
2023-11-11 21:38:35 -0500

    remove models

--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -1,7 +1,7 @@
 import { Observable, of } from 'rxjs';
 import { delay } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
-import { UpdateTodoPayload } from './models/Todos';
+import { UpdateTodoPayload } from '@hub-fx/examples';
 
 @Injectable({
   providedIn: 'root',
