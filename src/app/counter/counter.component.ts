34a0abc4e2889d84d0c0ac05c4c6f394f434408c
David Lai <laidav@gmail.com>
2023-11-11 20:58:29 -0500

    consume counter example

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,50 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { HubFactory, Reducer, Action } from '@hub-fx/core';
-import { Observable } from 'rxjs';
-
-// Actions
-const INCREMENT = 'INCREMENT';
-const increment = (): Action => ({ type: INCREMENT });
-
-const RESET = 'RESET';
-const reset = (): Action => ({ type: RESET });
-
-// Reducer function to handle state updates
-const countReducer: Reducer<{ count: number }> = (
-  state = { count: 0 },
-  action
-) => {
-  switch (action?.type) {
-    case INCREMENT:
-      return { count: state.count + 1 };
-    case RESET:
-      return { count: 0 };
-    default:
-      return state;
-  }
-};
+import { Component } from '@angular/core';
+import { Counter } from '@hub-fx/examples';
 
 @Component({
   selector: 'app-counter',
   templateUrl: './counter.component.html',
   styleUrls: ['./counter.component.scss'],
 })
-export class CounterComponent implements OnInit {
-  // Initialize a hub
-  hub = HubFactory();
-
-  state$: Observable<{ count: number }> | undefined;
-
-  increment() {
-    this.hub.dispatch(increment());
-  }
-
-  reset() {
-    this.hub.dispatch(reset());
-  }
-
-  ngOnInit() {
-    // Create observable stream
-    this.state$ = this.hub.store({ reducer: countReducer });
-  }
+export class CounterComponent {
+  counter = Counter();
+  state$ = this.counter.state$;
+  increment = this.counter.actions['increment'];
+  reset = this.counter.actions['reset'];
 }
